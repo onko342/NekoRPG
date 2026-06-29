8b802e7240f01992f3bbb3ed527b53ab935c1b31
onko342 <117436834+onko342@users.noreply.github.com>
2026-06-30 01:01:57 +0800

    attempt 5

--- src/main.js
+++ src/main.js
@@ -4016,7 +4016,7 @@ function load(save_data) {
         add_bestiary_lines(11);
         Object.keys(save_data["enemy_killcount"]).forEach(enemy_name => {
             const resolved_enemy_killcount_key = resolve_enemy_killcount(enemy_name);
-            enemy_killcount[enemy_name] = save_data["enemy_killcount"][enemy_name];
+            enemy_killcount[resolved_enemy_killcount_key] = save_data["enemy_killcount"][enemy_name];
             create_new_bestiary_entry(resolved_enemy_killcount_key);
             add_bestiary_zones(resolved_enemy_killcount_key);
 
